d116f8a3b378171ec3285ea327d81894c6f4e210
budRich <robstenklippa@gmail.com>
2023-09-03 10:19:53 +0000

    only include used stderr functions (ERX)

--- func/ERR.sh
+++ func/ERR.sh
@@ -4,11 +4,5 @@ set -E
 trap '[ "$?" -ne 98 ] || exit 98' ERR
 
 ERX() { >&2 echo  "[ERROR] $*" ; exit 98 ;}
-ERR() { >&2 echo  "[WARNING] $*"  ;}
-ERM() { >&2 echo  "$*"  ;}
-ERH(){
-  ___printhelp >&2
-  [[ -n "$*" ]] && printf '\n%s\n' "$*" >&2
-  exit 98
-}
-
+# ERR() { >&2 echo  "[WARNING] $*"  ;}
+# ERM() { >&2 echo  "$*"  ;}
